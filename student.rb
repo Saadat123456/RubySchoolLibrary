8b6c69180f6ba7b714f134fadb7acc5b214649ee
AlejandroNeira98 <ja.neira@uniandes.edu.co>
2022-05-19 18:56:41 -0500

    Fix requires

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
-require 'person'
+require './person'
 
 # Student class that extends from person
-class Student < person
+class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permision: true)
     super(age, name, parent_permision: parent_permision)
     @classroom = classroom
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
-require 'person'
+require './person'
 
 # Teacher class that extends from person
-class Teacher < person
+class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permision: true)
     super(age, name, parent_permision: parent_permision)
     @specialization = specialization
